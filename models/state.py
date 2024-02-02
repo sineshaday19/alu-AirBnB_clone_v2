cf044a99402709221f82c7ddda4f6adff2b55dee
Your Name <sineshaday19@github.com>
2024-02-02 02:31:04 -0800

    edit content

--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class state(BaseModel, Base):
+    """This is the class for state
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities  = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="states")
