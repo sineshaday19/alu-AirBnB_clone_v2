b857719cf7dec1985ef682edf3f9a78a5a5626ef
Your Name <sineshaday19@github.com>
2024-02-10 12:01:59 -0800

    solved

---
+++ installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip3 install pyparsing
+pip3 install appdirs
+pip3 install setuptools==40.1.0
+pip3 install cryptography==2.8
+pip3 install bcrypt==3.1.7
+pip3 install PyNaCl==1.3.0
+pip3 install Fabric3==1.14.post1
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
